2fe89285571b213ad1bb6e0e8abf458bffbdef2a
Sebastian Kirsch <sebastiankirsch@users.noreply.github.com>
2024-12-10 18:17:45 +0100

    Day3, part2

--- src/main/kotlin/io/github/sebastiankirsch/aoc2024/Day3.kt
+++ src/main/kotlin/io/github/sebastiankirsch/aoc2024/Day3.kt
@@ -1,10 +1,28 @@
 package io.github.sebastiankirsch.aoc2024
 
 fun main() {
-    var scanner = scannerForInputOf(object {}.javaClass)
-    var result = scanner.findAll("mul\\((\\d{1,3}),(\\d{1,3})\\)")
+    var result = scannerForInputOf(object {}.javaClass).findAll("(?:mul\\((\\d{1,3}),(\\d{1,3})\\))")
         .map { it.group(1).toInt() * it.group(2).toInt() }
         .reduce(0) { a, b -> a + b }
-
     println("Sum: $result")
+
+    var enabled = true
+    result =
+        scannerForInputOf(object {}.javaClass).findAll("do\\(()\\)|don't\\(()\\)|(?:mul\\((\\d{1,3}),(\\d{1,3})\\))")
+            .map {
+                if (it.group(1) != null) {
+                    enabled = true
+                    0
+                } else if (it.group(2) != null) {
+                    enabled = false
+                    0
+                } else if (enabled) {
+                    it.group(3).toInt() * it.group(4).toInt()
+                } else {
+                    0
+                }
+            }
+            .reduce(0) { a, b -> a + b }
+
+    println("Sum of enabled only: $result")
 }
